d3331985dabebd20e7f37651074d2b4e9a2f9244
asya-korobka <117941992+asya-korobka@users.noreply.github.com>
2023-04-24 18:25:48 +0300

    Add files via upload

---
+++ Practic 1/Practic 1/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Globalization;
+
+
+
+
+
+namespace ConsoleApp1
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+
+
+            Console.WriteLine("Введiть число X: ");
+
+            string number = Console.ReadLine();
+
+            if (string.IsNullOrWhiteSpace(number) || !int.TryParse(number, out int x))
+
+            {
+
+                Console.WriteLine("Введенi данi не являються числом, або не є цiлим числом");
+
+                Console.ReadLine();
+
+                return;
+
+            }
+
+            int fnum = 1;
+
+            double y = 0;
+
+            if (x <= 5)
+
+            {
+
+                fnum = 1;
+
+                y = (Math.Pow(x, 3) / 3) + 10;
+
+
+
+            }
+
+            else if (x > 6 && x <= 12)
+
+            {
+
+                fnum = 2;
+
+                double pr = 1;
+
+                for (int i = 7; i <= x; i++)
+
+                {
+
+                    pr *= Math.Pow((i - 4), 2);
+
+                }
+
+                y = 6 * pr;
+
+            }
+
+            else
+
+            {
+
+                fnum = 3;
+
+                double sum = 0;
+
+                for (int i = 13; i <= x; i++)
+
+                {
+
+                    sum += Math.Pow(i, 3) / 3;
+
+                }
+
+                y = sum;
+
+            }
+
+            Console.WriteLine($"Y рахується за {fnum} формулою\nY={y}");
+
+
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
---
+++ Practic 1/Task2/Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Globalization;
+
+
+
+
+
+namespace ConsoleApp1
+
+{
+
+    class Program
+
+    {
+
+
+
+        static string IntToBinary(int number)
+
+        {
+
+
+
+            int remainder;
+
+            string result = string.Empty;
+
+            while (number > 0)
+
+            {
+
+                remainder = number % 2;
+
+                number /= 2;
+
+                result = remainder.ToString() + result;
+
+            }
+
+            return result;
+
+        }
+
+
+
+        static string IntToOctal(int number)
+
+        {
+
+
+
+            int Octal = 0, i = 1;
+
+            string result = string.Empty;
+
+            for (int j = number; j > 0; j = j / 8)
+
+            {
+
+                Octal = Octal + (j % 8) * i;
+
+                i *= 10;
+
+                number /= 8;
+
+                result = Octal.ToString();
+
+            }
+
+            return result;
+
+        }
+
+
+
+
+
+        static void Main(string[] args)
+
+        {
+
+
+
+            char key;
+
+            do
+
+            {
+
+                Console.WriteLine("\nПрограма переведення числа з десяткової системи числення\n" +
+
+                    "Оберiть систему числення:\n1. Двiйкова\n2. Восьмирична\nn. Вихiд");
+
+
+
+                key = Console.ReadKey().KeyChar;
+
+
+
+                if (key == '1' || key == '2')
+
+                {
+
+                    Console.WriteLine("\nВведiть число для конвертацiї: ");
+
+                    var numberString = Console.ReadLine();
+
+                    if (!int.TryParse(numberString, out var number))
+
+                    {
+
+                        Console.WriteLine("Введенi даннi не цiле число!!!");
+
+                        continue;
+
+                    }
+
+                    var result = key == '1' ? IntToBinary(number) : IntToOctal(number);
+
+                    Console.WriteLine($"Результат: {result}");
+
+                }
+
+
+
+
+
+
+
+            } while (key != 'n');
+
+            Console.ReadLine();
+
+        }
+
+    }
+
+}
